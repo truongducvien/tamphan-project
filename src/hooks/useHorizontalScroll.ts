f412d02b0f3699ff1ba5b68e353a625490f7b264
Tam Minh Phan <tam.phan@3forcom.com.vn>
2022-08-21 15:50:17 +0700

    🐛 fix(fix): clean up code

---
+++ src/hooks/useHorizontalScroll.ts
@@ -0,0 +1,22 @@
+import { useRef, useEffect } from 'react';
+
+export const useHorizontalScroll = () => {
+	const elRef = useRef<HTMLDivElement>(null);
+	// eslint-disable-next-line consistent-return
+	useEffect(() => {
+		const el = elRef.current;
+		if (el) {
+			const onWheel = (e: WheelEvent) => {
+				if (e.deltaY === 0) return;
+				e.preventDefault();
+				el.scrollTo({
+					left: el.scrollLeft + e.deltaY,
+					behavior: 'smooth',
+				});
+			};
+			el.addEventListener('wheel', onWheel);
+			return () => el.removeEventListener('wheel', onWheel);
+		}
+	}, []);
+	return elRef;
+};
