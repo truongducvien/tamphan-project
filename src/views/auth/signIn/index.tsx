bec398137476d10919ab52db746e7a2c0f9c0aa2
tam.phanminh <tam.phanminh@novatechgroup.vn>
2022-09-16 15:08:33 +0700

    🐛 fix(login): use localSttorage

--- src/views/auth/signIn/index.tsx
+++ src/views/auth/signIn/index.tsx
@@ -35,7 +35,6 @@ const SignIn: React.FC = () => {
 
 	const usernameRef = useRef<HTMLInputElement>(null);
 	const passRef = useRef<HTMLInputElement>(null);
-	const checkboxRef = useRef<HTMLInputElement>(null);
 
 	const dispatch = useAppDispatch();
 	const { logined } = useAppSelector(state => state.user);
@@ -56,7 +55,7 @@ const SignIn: React.FC = () => {
 			return;
 		}
 		setError({ username: '', password: '' });
-		dispatch(userLogin(usernameRef.current?.value || '', passRef.current?.value || '', checkboxRef.current?.checked));
+		dispatch(userLogin(usernameRef.current?.value || '', passRef.current?.value || '', true));
 	};
 
 	const [show, setShow] = React.useState(false);
@@ -142,12 +141,6 @@ const SignIn: React.FC = () => {
 							{errorMessase.password || errorMessase.username || (error['users/LOGIN'] as string)}
 						</Text>
 						<Flex justifyContent="space-between" align="center" mb="24px">
-							<FormControl display="flex" alignItems="center">
-								<Checkbox ref={checkboxRef} id="remember-login" colorScheme="brandScheme" me="10px" />
-								<FormLabel htmlFor="remember-login" mb="0" fontWeight="normal" color={textColor} fontSize="sm">
-									Duy trì đăng nhập
-								</FormLabel>
-							</FormControl>
 							<NavLink to="/auth/forgot-password">
 								<Text color={textColorBrand} fontSize="sm" w="124px" fontWeight="500">
 									Quên mật khẩu?
